5f358dab644e49295f6c63952eb3b9ce0f406d97
Дмитрий Судариков <grohopuz@gmail.com>
2025-03-24 02:20:00 +0300

    adding my files to my repository

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+var myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+  var mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/honor70.png") {
+    myImage.setAttribute("src", "images/honor90.png");
+  } else if (mySrc === "images/honor90.png") {
+    myImage.setAttribute("src", "images/honor100.png");
+  } else if (mySrc === "images/honor100.png") {
+    myImage.setAttribute("src", "images/honor200.png");
+  } else if (mySrc === "images/honor200.png") {
+    myImage.setAttribute("src", "images/honor300.png");
+  } else {
+    myImage.setAttribute("src", "images/honor70.png");
+  }
+};
+
+var myButton = document.querySelector("button");
+var myHeading = document.querySelector("h4");
+
+function setUserName() {
+  var myName = prompt("Please enter you name.");
+  localStorage.setItem("name", myName);
+  myHeading.textContent = "Hi, " + myName + "! Welcom to Honor!";
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem("name");
+  myHeading.textContent = "Hi, " + storedName + "! Welcom to Honor!";
+}
+
+myButton.onclick = function () {
+  setUserName();
+};
